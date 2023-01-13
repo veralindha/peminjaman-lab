c895a314ef4ccd51b8c4182109b4623215c9fa19
veralindha <fangkyistiawan@gmail.com>
2023-01-13 09:36:09 +0700

    repair color element guru

--- components/jadwal/table_schedule.js
+++ components/jadwal/table_schedule.js
@@ -5,30 +5,37 @@ export default function TabelSchedule() {
         <h2 className="section-title">Kode Warna</h2>
         <div className="section-lead">
           <div className="row m-2">
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'red', padding: '10px' }} id='1'></div>
-            <label htmlFor="1" className="font-weight-bold text-dark m-1 mr-3">Alex</label>
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'blue', padding: '10px' }} id='2'></div>
-            <label htmlFor="2" className="font-weight-bold text-dark m-1 mr-3">Dendi</label>
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'green', padding: '10px' }} id='3'></div>
-            <label htmlFor="3" className="font-weight-bold text-dark m-1 mr-3">Fuad</label>
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'blue', padding: '10px' }} id='4'></div>
-            <label htmlFor="4" className="font-weight-bold text-dark m-1 mr-3">Herlambang</label>
+            <div className="col-6 d-flex justify-content-between">
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'red', padding: '10px' }} id='1'></div>
+              <label htmlFor="1" className="font-weight-bold text-dark m-1">Alex</label>
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'blue', padding: '10px' }} id='2'></div>
+              <label htmlFor="2" className="font-weight-bold text-dark m-1 ">Dendi</label>
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'green', padding: '10px' }} id='3'></div>
+              <label htmlFor="3" className="font-weight-bold text-dark m-1 ">Fuad</label>
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'blue', padding: '10px' }} id='4'></div>
+              <label htmlFor="4" className="font-weight-bold text-dark m-1 ">Herlambang</label>
+            </div>
           </div>
           <div className="row m-2">
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'red', padding: '10px' }} id='5'></div>
-            <label htmlFor="5" className="font-weight-bold text-dark m-1 mr-3">Hilal</label>
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'blue', padding: '10px' }} id='6'></div>
-            <label htmlFor="6" className="font-weight-bold text-dark m-1 mr-3">Reza</label>
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'green', padding: '10px' }} id='7'></div>
-            <label htmlFor="7" className="font-weight-bold text-dark m-1 mr-3">Yogi</label>
-            <div className="col-1 m-1 rounded" style={{ backgroundColor: 'green', padding: '10px' }} id='8'></div>
-            <label htmlFor="8" className="font-weight-bold text-dark m-1 mr-3">Zeta</label>
+            <div className="col-6 d-flex justify-content-between">
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'red', padding: '10px' }} id='5'></div>
+              <label htmlFor="5" className="font-weight-bold text-dark m-1 ">Hilal</label>
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'blue', padding: '10px' }} id='6'></div>
+              <label htmlFor="6" className="font-weight-bold text-dark m-1 ">Reza</label>
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'green', padding: '10px' }} id='7'></div>
+              <label htmlFor="7" className="font-weight-bold text-dark m-1 ">Yogi</label>
+              <div className="col-1 m-1 rounded" style={{ backgroundColor: 'green', padding: '10px' }} id='8'></div>
+              <label htmlFor="8" className="font-weight-bold text-dark m-1 ">Zeta</label>
+            </div>
           </div>
         </div>
       </div>
       <div className="card">
-        <div className="card-header">
+        <div className="card-header d-flex justify-content-between">
           <h4>Tabel Jadwal</h4>
+          <button type="button" className="btn btn-primary btn-floating">
+            <i className="fas fa-print" />
+          </button>
         </div>
         <div className="card-body">
           <div>
